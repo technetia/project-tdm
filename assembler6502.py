9c07f8fdb9c955f49cf6ff92a25b1c0629157811
Nicholas Kim <foehammer.nickel@gmail.com>
2013-11-13 14:41:25 -0500

    Use stdin for assembler input

--- assembler6502.py
+++ assembler6502.py
@@ -5,20 +5,15 @@ import assembler6502_tokenizer as tokenizer
 import assembler6502_parser as parser
 
 def output_byte(hexcode):
-    sys.stdout.write(hexcode)
+    sys.stdout.write(hexcode + "\n")
     #sys.stdout.write(chr(int(hexcode, 16)))
 
 def main():
-    code = """
-; sample code
-beginning:
-    sty $44,X
-    """
+    code = sys.stdin.read()
     for line in code.split("\n"):
         hexcodes = parser.parse_line(line)
         for hexcode in hexcodes:
             output_byte(hexcode)
-    print
 
 if __name__ == "__main__":
     main()
